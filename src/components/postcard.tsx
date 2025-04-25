d770bd853e01db88e6cb6d9c1fbcfddfbcd2ae1b
BiTech5 <bikramkhatri050@gmail.com>
2025-04-25 18:49:53 +0545

    update layout

--- src/components/postcard.tsx
+++ src/components/postcard.tsx
@@ -1,37 +1,41 @@
 const Cards = () => {
-    return (
-        <div className="flex gap-4 flex-col md:flex-row">
-            <div className="card bg-base-100 w-96 shadow-sm">
-                <figure>
-                    <img
-                        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                        alt="Shoes" />
-                </figure>
-                <div className="card-body">
-                    <h2 className="card-title">Card Title</h2>
-                    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Read More</button>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div>
 
-            <div className="card bg-base-100 w-96 shadow-sm">
-                <figure>
-                    <img
-                        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                        alt="Shoes" />
-                </figure>
-                <div className="card-body">
-                    <h2 className="card-title">Card Title</h2>
-                    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Read More</button>
-                    </div>
-                </div>
+      <h1 className="text-white text-3xl font-bold mb-2">Blogs</h1>
+      <div className="flex gap-4 flex-col md:flex-row">
+        <div className="card bg-base-100 w-96 shadow-sm">
+          <figure>
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+              alt="Shoes" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Card Title</h2>
+            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Read More</button>
             </div>
+          </div>
         </div>
-    )
+
+        <div className="card bg-base-100 w-96 shadow-sm">
+          <figure>
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+              alt="Shoes" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Card Title</h2>
+            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Read More</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Cards;
